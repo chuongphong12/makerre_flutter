ccffdb7e87da2efd6bf569962947c30168cd54ff
BM Hudson <ttnhan@brickmate.vn>
2022-09-07 14:22:22 +0700

    complete login UI

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -19,6 +19,10 @@ class _LoginScreenState extends State<LoginScreen> {
           image: DecorationImage(
             image: AssetImage('assets/images/BG.png'),
             fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Color.fromARGB(100, 0, 0, 0),
+              BlendMode.darken,
+            ),
           ),
         ),
         child: Padding(
@@ -26,13 +30,13 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const SizedBox(height: 150),
+              const SizedBox(height: 108),
               SvgPicture.asset(
                 'assets/images/icons/Logo.svg',
                 width: 166,
                 fit: BoxFit.contain,
               ),
-              const SizedBox(height: 150),
+              const SizedBox(height: 104),
               FormBuilder(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -45,7 +49,23 @@ class _LoginScreenState extends State<LoginScreen> {
                           ),
                     ),
                     const SizedBox(height: 8),
-                    FormBuilderTextField(name: 'email'),
+                    FormBuilderTextField(
+                      name: 'email',
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(width: 1),
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        filled: false,
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFFBDBDBD)),
+                        ),
+                      ),
+                      style: Theme.of(context).textTheme.headline6!.copyWith(
+                            color: const Color(0xFFFFFFFF),
+                            fontSize: 16,
+                          ),
+                    ),
                     const SizedBox(height: 28),
                     Text(
                       '이메일',
@@ -55,13 +75,39 @@ class _LoginScreenState extends State<LoginScreen> {
                           ),
                     ),
                     const SizedBox(height: 8),
-                    FormBuilderTextField(name: 'email'),
+                    FormBuilderTextField(
+                      name: 'password',
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(width: 1),
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        filled: false,
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFFBDBDBD)),
+                        ),
+                      ),
+                      obscureText: true,
+                      style: Theme.of(context).textTheme.headline6!.copyWith(
+                            color: const Color(0xFFFFFFFF),
+                            fontSize: 16,
+                          ),
+                    ),
                   ],
                 ),
               ),
               const SizedBox(height: 32),
               ElevatedButton(
                 onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(double.maxFinite, 52),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  backgroundColor: Theme.of(context).primaryColor,
+                  foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                  textStyle: Theme.of(context).textTheme.headline4,
+                ),
                 child: const Text('로그인'),
               ),
               const SizedBox(height: 16),
@@ -69,27 +115,107 @@ class _LoginScreenState extends State<LoginScreen> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      '이메일로 가입',
-                      style: Theme.of(context).textTheme.headline4!.copyWith(
-                            color: const Color(0xFFE0E0E0),
-                          ),
+                    TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(horizontal: 2),
+                      ),
+                      child: Text(
+                        '이메일로 가입',
+                        style: Theme.of(context).textTheme.headline4!.copyWith(
+                              color: const Color(0xFFE0E0E0),
+                              fontWeight: FontWeight.w400,
+                            ),
+                      ),
                     ),
                     const VerticalDivider(
                       color: Color(0xFFE0E0E0),
-                      thickness: 3,
-                      indent: 20,
-                      endIndent: 0,
-                      width: 20,
+                      thickness: 1,
+                      indent: 13,
+                      endIndent: 13,
+                      width: 24,
                     ),
-                    Text(
-                      '비밀번호 찾기',
-                      style: Theme.of(context).textTheme.headline4!.copyWith(
-                            color: const Color(0xFFE0E0E0),
-                          ),
-                    )
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(horizontal: 2),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        '비밀번호 찾기',
+                        style: Theme.of(context).textTheme.headline4!.copyWith(
+                              color: const Color(0xFFE0E0E0),
+                              fontWeight: FontWeight.w400,
+                            ),
+                      ),
+                    ),
                   ],
                 ),
+              ),
+              const SizedBox(height: 110),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      backgroundColor: const Color(0xFFFFE812),
+                      padding: const EdgeInsets.all(15),
+                    ),
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.train,
+                      size: 30,
+                      color: Colors.black,
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      backgroundColor: const Color(0xFF000000),
+                      padding: const EdgeInsets.all(15),
+                    ),
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.train,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      backgroundColor: const Color(0xFF2BDB71),
+                      padding: const EdgeInsets.all(15),
+                    ),
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.train,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      backgroundColor: const Color(0xFFFFFFFF),
+                      padding: const EdgeInsets.all(15),
+                    ),
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.train,
+                      size: 30,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Text(
+                'SNS 간편로그인',
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6!
+                    .copyWith(color: Colors.white),
               )
             ],
           ),
