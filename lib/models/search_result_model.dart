a5ec97b19047fdb4237fafead1b66050f3007784
BM Hudson <ttnhan@brickmate.vn>
2022-09-09 17:02:52 +0700

    add search result grid view

---
+++ lib/models/search_result_model.dart
@@ -0,0 +1,79 @@
+class SearchResult {
+  final String image;
+  final String title;
+  final String description;
+  final double price;
+  final double reviewCount;
+  final double rating;
+  final bool isBookmark;
+
+  SearchResult({
+    required this.image,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.reviewCount,
+    required this.rating,
+    required this.isBookmark,
+  });
+
+  static final List<SearchResult> searchResults = [
+    SearchResult(
+      image: 'assets/images/search_result/celine-bag.png',
+      title: '픽스 마스터',
+      isBookmark: true,
+      description:
+          'Old Celine, Celine을 전문으로 합니다. 다양한 사이즈의 Old Celine, Celine을 전문으로 합니다. 다양한 사이즈의',
+      price: 500000,
+      rating: 4.9,
+      reviewCount: 108,
+    ),
+    SearchResult(
+      image: 'assets/images/search_result/t-shirt.png',
+      title: '세탁 특공',
+      isBookmark: true,
+      description: '명품 세탁 1위 비결은 깔끔함에 있습니다. 세탁 특공은',
+      price: 450000,
+      rating: 4.8,
+      reviewCount: 80,
+    ),
+    SearchResult(
+      image: 'assets/images/search_result/blanket.png',
+      title: '수리수리 마수리',
+      isBookmark: false,
+      description: '명품 세탁 1위 비결은 깔끔함에 있습니다. 세탁 특공은 ',
+      price: 680000,
+      rating: 4.6,
+      reviewCount: 222,
+    ),
+    SearchResult(
+      image: 'assets/images/search_result/coffee.png',
+      title: '리폼의 신',
+      isBookmark: false,
+      description: 'Old Celine, Celine을 전문으로 합니다. 다양한 사이즈의',
+      price: 230000,
+      rating: 4.9,
+      reviewCount: 65,
+    ),
+    SearchResult(
+      image: 'assets/images/search_result/noddle.png',
+      title: '수리수리 마수리',
+      isBookmark: false,
+      description:
+          'Old Celine, Celine을 전문으로 합니다. 다양한 사이즈의 Old Celine, Celine을 전문으로 합니다. 다양한 사이즈의',
+      price: 125000,
+      rating: 4.4,
+      reviewCount: 9,
+    ),
+    SearchResult(
+      image: 'assets/images/search_result/old-guy.png',
+      title: '수선 매니아',
+      isBookmark: false,
+      description:
+          'Old Celine, Celine을 전문으로 합니다. 다양한 사이즈의 Old Celine, Celine을 전문으로 합니다. 다양한 사이즈의',
+      price: 310000,
+      rating: 4.8,
+      reviewCount: 201,
+    )
+  ];
+}
--- lib/screens/home/search/search_result_screen.dart
+++ lib/screens/home/search/search_result_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
+import 'package:makerre_flutter/models/search_result_model.dart';
 import 'package:makerre_flutter/widgets/app_drawer.dart';
 
 class SearchResultScreen extends StatelessWidget {
@@ -22,128 +23,218 @@ class SearchResultScreen extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: const AppDrawer(),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              IconButton(
-                onPressed: () {
-                  GoRouter.of(context).goNamed('home');
-                },
-                icon: const Icon(Icons.arrow_back_ios_new),
-              ),
-              Expanded(
-                child: ListTile(
-                  title: Text(result),
-                  trailing: Icon(
-                    Icons.search,
-                    size: 28,
-                    color: Theme.of(context).colorScheme.onError,
-                  ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    GoRouter.of(context).goNamed('home');
+                  },
+                  icon: const Icon(Icons.arrow_back_ios_new),
                 ),
-              )
-            ],
-          ),
-          Divider(
-            thickness: 1,
-            height: 1,
-            color: Theme.of(context).primaryColor,
-          ),
-          const SizedBox(height: 28),
-          hasResult == true
-              ? Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            '총 123건',
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                          Material(
-                            shape: RoundedRectangleBorder(
-                              side: const BorderSide(
-                                width: 1,
-                                color: Color(0xFFBDBDBD),
-                              ),
-                              borderRadius: BorderRadius.circular(20),
+                Expanded(
+                  child: ListTile(
+                    title: Text(result),
+                    trailing: Icon(
+                      Icons.search,
+                      size: 28,
+                      color: Theme.of(context).colorScheme.onError,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Divider(
+              thickness: 1,
+              height: 1,
+              color: Theme.of(context).primaryColor,
+            ),
+            const SizedBox(height: 28),
+            hasResult == true
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              '총 123건',
+                              style: Theme.of(context).textTheme.headline6,
                             ),
-                            child: InkWell(
-                              borderRadius: BorderRadius.circular(20),
-                              onTap: () {
-                                showModalBottomSheet(
-                                  context: context,
-                                  enableDrag: true,
-                                  constraints: const BoxConstraints.tightFor(
-                                      height: 174, width: double.infinity),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(12),
+                            Material(
+                              shape: RoundedRectangleBorder(
+                                side: const BorderSide(
+                                  width: 1,
+                                  color: Color(0xFFBDBDBD),
+                                ),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: InkWell(
+                                borderRadius: BorderRadius.circular(20),
+                                onTap: () {
+                                  _buildBottomModal(context);
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 10,
+                                    vertical: 5,
                                   ),
-                                  builder: (context) => Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                      vertical: 10,
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        '거리순',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText1,
+                                      ),
+                                      const Icon(
+                                        Icons.keyboard_arrow_down,
+                                        size: 20,
+                                        color: Colors.black,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: 28),
+                        GridView.count(
+                          crossAxisCount: 2,
+                          shrinkWrap: true,
+                          crossAxisSpacing: 15,
+                          childAspectRatio: 80 / 135,
+                          mainAxisSpacing: 20,
+                          physics: const NeverScrollableScrollPhysics(),
+                          children: SearchResult.searchResults
+                              .map(
+                                (val) => Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Image.asset(
+                                      val.image,
+                                      width: double.maxFinite,
+                                      fit: BoxFit.cover,
                                     ),
-                                    child: Column(
-                                      mainAxisSize: MainAxisSize.min,
+                                    const SizedBox(height: 10),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
                                       children: [
-                                        TextButton(
-                                          onPressed: () {},
-                                          child: const Text('평점순'),
+                                        Text(
+                                          val.title,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headline4,
                                         ),
-                                        TextButton(
-                                          onPressed: () {},
-                                          child: const Text('평점순'),
+                                        val.isBookmark
+                                            ? const Icon(Icons.bookmark)
+                                            : const Icon(
+                                                Icons.bookmark_outline),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 12),
+                                    Text(
+                                      val.description,
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                      style:
+                                          Theme.of(context).textTheme.headline6,
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      '${val.price.toStringAsFixed(0)} 원 ~',
+                                      style:
+                                          Theme.of(context).textTheme.headline4,
+                                    ),
+                                    const SizedBox(height: 6),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            const Icon(
+                                              Icons.star_rate_rounded,
+                                              size: 17,
+                                              color: Colors.amber,
+                                            ),
+                                            Text(
+                                              val.rating.toStringAsFixed(1),
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyText1,
+                                            ),
+                                          ],
                                         ),
-                                        TextButton(
-                                          onPressed: () {},
-                                          child: const Text('평점순'),
+                                        Text(
+                                          '${val.reviewCount.toStringAsFixed(0)} 리뷰',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyText1,
                                         ),
                                       ],
                                     ),
-                                  ),
-                                );
-                              },
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 10,
-                                  vertical: 5,
-                                ),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      '거리순',
-                                      style:
-                                          Theme.of(context).textTheme.bodyText1,
-                                    ),
-                                    const Icon(
-                                      Icons.keyboard_arrow_down,
-                                      size: 20,
-                                      color: Colors.black,
-                                    ),
                                   ],
                                 ),
-                              ),
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                )
-              : Center(
-                  child: Text(
-                    '\'$result\' 에 해당하는 결과값이 없습니다.',
-                    style: Theme.of(context).textTheme.headline4!.copyWith(
-                          color: const Color(0xFFBDBDBD),
-                          fontWeight: FontWeight.normal,
+                              )
+                              .toList(),
                         ),
-                  ),
-                )
-        ],
+                        const SizedBox(height: 20),
+                      ],
+                    ),
+                  )
+                : Center(
+                    child: Text(
+                      '\'$result\' 에 해당하는 결과값이 없습니다.',
+                      style: Theme.of(context).textTheme.headline4!.copyWith(
+                            color: const Color(0xFFBDBDBD),
+                            fontWeight: FontWeight.normal,
+                          ),
+                    ),
+                  )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<dynamic> _buildBottomModal(BuildContext context) {
+    return showModalBottomSheet(
+      context: context,
+      enableDrag: true,
+      constraints:
+          const BoxConstraints.tightFor(height: 174, width: double.infinity),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      builder: (context) => Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextButton(
+              onPressed: () {},
+              child: const Text('평점순'),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text('평점순'),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text('평점순'),
+            ),
+          ],
+        ),
       ),
     );
   }
